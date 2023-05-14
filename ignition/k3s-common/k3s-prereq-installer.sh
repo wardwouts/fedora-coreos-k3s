eb531b75763e93c093ff86de1b045ca3e9f7df01
wardwouts <ward@wouts.nl>
2023-05-14 18:57:28 +0200

    selinux-policy-base is already installed in fcos

--- ignition/k3s-common/k3s-prereq-installer.sh
+++ ignition/k3s-common/k3s-prereq-installer.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 main() {
   #rpm-ostree install container-selinux
-  rpm-ostree install selinux-policy-base
+  #rpm-ostree install selinux-policy-base
   rpm-ostree install https://rpm.rancher.io/k3s/latest/common/centos/7/noarch/k3s-selinux-0.2-1.el7_8.noarch.rpm
   return 0
 }
